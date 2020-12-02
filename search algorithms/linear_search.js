ec02a1f4a1c6ac8103298f6178ebc48dbb79cd7d
Hamzah-Ahmad <50108683+Hamzah-Ahmad@users.noreply.github.com>
2020-12-02 12:03:57 -0600

    Added time complexity and explanation for linear search

--- search algorithms/linear_search.js
+++ search algorithms/linear_search.js
@@ -1,3 +1,6 @@
+//Time Complexity: O(n)
+//Explanation: The algorithm goes through each element of the array in a linear fashion to search for the specified value
+
 function linear_search(arr, val){
     for(let i = 0; i < arr.length; i++){
         if(arr[i] === val){
