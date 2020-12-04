a9a8f92ee78d896817b3b56d3effe5910aa828a4
Hamzah-Ahmad <50108683+Hamzah-Ahmad@users.noreply.github.com>
2020-12-03 23:38:08 -0600

    Added additional commentsbubble_sort

--- sorting algorithms/bubble_sort.js
+++ sorting algorithms/bubble_sort.js
@@ -17,7 +17,6 @@
 //Optimized version
 function bubbleSort(arr) {
   for (let i = arr.length; i > 0; i--) {
-    //the second for loop reduces the number of comparisons each iteration. For an array of length 5 ,for example, the algorithm makes 4 comparisons. In the next iteration, it makes three comparisons.
     for (j = 0; j < i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         var temp = arr[j];
