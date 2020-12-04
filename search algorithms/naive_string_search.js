eddfa1527366dfc58f8aedec3b43960327fe150b
Hamzah-Ahmad <50108683+Hamzah-Ahmad@users.noreply.github.com>
2020-12-03 23:35:45 -0600

    Added additional explanation comment for naive_string_search

--- search algorithms/naive_string_search.js
+++ search algorithms/naive_string_search.js
@@ -1,22 +1,21 @@
 //Time complexity: O(m * n). The m is the size of pattern and n is the size of the main string
 //Explanation: This algorithm checks for the presence of a string pattern in a larger string. For example, checking for the number of times the pattern "omg" appears in the string "wowomgzomg".
 
-function naiveStringSearch(longStr, pattern){
-    let count = 0;
-    for(let i = 0; i <longStr.length; i++){
-        for(let j = 0; j < pattern.length; j++){
-            if(pattern[j] !== longStr[i + j] ){
-                break;
-            }
-            //the if block below checks if the entire short pattern has been run. If that happens, it means that a break didn't occur, and so there's a match, so we increase the count
-            if(j=== pattern.length -1){
-                count++;
-            }
-        }
-        
+function naiveStringSearch(longStr, pattern) {
+  let count = 0;
+  for (let i = 0; i < longStr.length; i++) {
+    for (let j = 0; j < pattern.length; j++) {
+      if (pattern[j] !== longStr[i + j]) {
+        break;
+      }
+      //the if block below checks if the entire short pattern has been run. If that happens, it means that a break didn't occur, and so there's a match, so we increase the count. We have to put an if check, otherwise the count would increase for each character match, not for the whole pattern match.
+      if (j === pattern.length - 1) {
+        count++;
+      }
     }
-    return count;
+  }
+  return count;
 }
 
-let res = naiveStringSearch("wowomgzomgdsomg", "omg")
+let res = naiveStringSearch("wowomgzomgdsomg", "omg");
 console.log(res);
